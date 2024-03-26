c5f387e1febf69362db678e8d057871c9914ed0b
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-26 14:47:45 -0300

    exe 04 finalizado

---
+++ aula04/exe04/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exe 04</title>
+</head>
+<body>
+        <?php
+        $i = 1;
+        while ($i <= 10) {
+            $mult = $i * 5; 
+            echo "$i x 5 = $mult <br>";
+            $i++;
+        }
+        ?>
+    </form>
+</body>
+</html>
