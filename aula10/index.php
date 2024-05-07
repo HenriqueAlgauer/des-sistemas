25de285e920e46a75f1bc3e84743f4a0161e95dd
Pia <algauer.henrique@gmail.com>
2024-05-07 20:44:53 -0300

    aula 10

---
+++ aula10/index.php
@@ -0,0 +1,12 @@
+<?php 
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=teste', 'root', 'hhma2005');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->exec("INSERT INTO teste.seila (nome, estadoCivil, cpf, profissao) VALUES ('José da silva', 'Casado', 12399988871, 'Pedreiro')");
+
+    $sqlQuery = $pdo->prepare("INSERT INTO teste.seila (nome, estadoCivil, cpf, profissao) VALUES ('José da silva', 'Casado', 12399988871, 'Pedreiro')");
+}catch(PDOException $erro){
+    echo "ERRO => " . $erro->getMessage();
+}
+?>
